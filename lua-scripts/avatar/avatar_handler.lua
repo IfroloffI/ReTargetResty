947fc0a17ff7a54abe3d82b02f9be75731068c79
WonderBro <froloff1830@gmail.com>
2025-05-16 20:23:48 +0300

    RE-77: fix shared cache in conf and lua init

--- lua-scripts/avatar/avatar_handler.lua
+++ lua-scripts/avatar/avatar_handler.lua
@@ -2,22 +2,27 @@ local cjson = require "cjson"
 local sha1 = require "resty.sha1"
 local str = require "resty.string"
 
+-- INIT
+local cache = ngx.shared.avatar_cache
+if not cache then
+    ngx.log(ngx.ERR, "Failed to get shared dictionary 'avatar_cache'")
+    ngx.exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
+end
+
 local session_id = ngx.var.cookie_session_id
 if not session_id then
     ngx.exit(ngx.HTTP_UNAUTHORIZED)
 end
 
 local cache_key = "avatar_" .. ngx.md5(session_id)
-
-local cache = ngx.shared.avatar_cache
 local method = ngx.var.request_method
 
-if method == "GET" and ngx.var.uri:find("/download") then
+-- Обработка GET (download)
+if method == "GET" then
     local avatar_data = cache:get(cache_key)
 
     if avatar_data then
         local cached = cjson.decode(avatar_data)
-
         local etag = ngx.var.http_if_none_match
         if etag and etag == cached.etag then
             ngx.exit(304)
@@ -34,7 +39,8 @@ if method == "GET" and ngx.var.uri:find("/download") then
     return
 end
 
-if method == "PUT" and ngx.var.uri:find("/upload") then
+-- Обработка PUT (upload)
+if method == "PUT" then
     cache:delete(cache_key)
     ngx.log(ngx.NOTICE, "Purged avatar cache for: ", cache_key)
     return
