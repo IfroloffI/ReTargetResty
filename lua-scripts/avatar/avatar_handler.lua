1c19a9833316681b614c14a5219ea5b975d059de
WonderBro <froloff1830@gmail.com>
2025-05-17 01:04:25 +0300

    RE-77: go work

--- lua-scripts/avatar/avatar_handler.lua
+++ lua-scripts/avatar/avatar_handler.lua
@@ -6,30 +6,15 @@ if not session_id then
     ngx.exit(401)
 end
 
-local cache_key = "avatar_" .. ngx.md5(session_id)
-
 if ngx.var.request_method == "GET" then
+    local cache_key = "avatar_" .. ngx.md5(session_id)
     local cached = cache:get(cache_key)
     if cached then
         cache:set(cache_key, cached, 300)
         ngx.header["Content-Type"] = "image/jpeg"
         ngx.print(cached)
         ngx.exit(200)
-    else
-        local res = ngx.location.capture("/internal-avatar", {
-            method = ngx.HTTP_GET,
-            args = ngx.req.get_uri_args()
-        })
-        if res.status ~= 200 then
-            ngx.exit(res.status)
-        end
-        cache:set(cache_key, res.body, 300)
-        ngx.print(res.body)
-        ngx.exit(200)
     end
 elseif ngx.var.request_method == "PUT" then
     cache:delete(cache_key)
     ngx.exit(200)
-else
-    ngx.exit(405)
-end
