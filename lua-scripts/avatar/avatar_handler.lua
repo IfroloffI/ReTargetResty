1e0c353b4906c56807353116ec9fbad374d5889a
WonderBro <froloff1830@gmail.com>
2025-05-17 00:57:39 +0300

    RE-77: fix configs

--- lua-scripts/avatar/avatar_handler.lua
+++ lua-scripts/avatar/avatar_handler.lua
@@ -11,12 +11,24 @@ local cache_key = "avatar_" .. ngx.md5(session_id)
 if ngx.var.request_method == "GET" then
     local cached = cache:get(cache_key)
     if cached then
+        cache:set(cache_key, cached, 300)
         ngx.print(cached)
-        return ngx.exit(200)
+        ngx.exit(200)
+    else
+        local res = ngx.location.capture("/internal-avatar/", {
+            method = ngx.HTTP_GET,
+            args = ngx.req.get_uri_args()
+        })
+        if res.status ~= 200 then
+            ngx.exit(res.status)
+        end
+        cache:set(cache_key, res.body, 300)
+        ngx.print(res.body)
+        ngx.exit(200)
     end
-end
-
-if ngx.var.request_method == "PUT" then
+elseif ngx.var.request_method == "PUT" then
     cache:delete(cache_key)
     ngx.exit(200)
+else
+    ngx.exit(405)
 end
