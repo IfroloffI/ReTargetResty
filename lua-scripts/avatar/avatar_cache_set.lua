0331b6fc6f9dbddc2c4c12a1c434d030f1dba857
WonderBro <froloff1830@gmail.com>
2025-05-16 20:38:43 +0300

    RE-77: fix avatar cache

--- lua-scripts/avatar/avatar_cache_set.lua
+++ lua-scripts/avatar/avatar_cache_set.lua
@@ -1,6 +1,6 @@
 local cjson = require "cjson"
 
-if not ngx.ctx.cache_avatar or ngx.arg[2] ~= "" then
+if not ngx.ctx.should_cache or ngx.arg[2] ~= "" then
     return
 end
 
@@ -8,15 +8,22 @@ local cache = ngx.shared.avatar_cache
 local cache_key = ngx.ctx.cache_key
 local resp_data = ngx.arg[1]
 
-local compressed_data = resp_data -- TODO: Сжатие сделать
+local content_type = ngx.header["Content-Type"] or ""
+if not content_type:match("^image/") then
+    ngx.log(ngx.WARN, "Not caching non-image content: ", content_type)
+    return
+end
 
 local to_cache = {
-    data = compressed_data,
-    content_type = ngx.header["Content-Type"],
+    data = resp_data,
+    content_type = content_type,
     etag = ngx.header["ETag"] or ngx.md5(resp_data),
     timestamp = ngx.time()
 }
 
-cache:set(cache_key, cjson.encode(to_cache), 300)
-
-ngx.log(ngx.NOTICE, "Cached avatar for: ", cache_key)
+local success, err = cache:set(cache_key, cjson.encode(to_cache), 300)
+if not success then
+    ngx.log(ngx.ERR, "Failed to cache avatar: ", err)
+else
+    ngx.log(ngx.NOTICE, "Successfully cached avatar for key: ", cache_key, " Content-Type: ", content_type)
+end
--- lua-scripts/avatar/avatar_handler.lua
+++ lua-scripts/avatar/avatar_handler.lua
@@ -1,45 +1,45 @@
 local cjson = require "cjson"
-local sha1 = require "resty.sha1"
-local str = require "resty.string"
-
--- INIT
 local cache = ngx.shared.avatar_cache
+
 if not cache then
     ngx.log(ngx.ERR, "Failed to get shared dictionary 'avatar_cache'")
-    ngx.exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
+    return ngx.exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
 end
 
 local session_id = ngx.var.cookie_session_id
 if not session_id then
-    ngx.exit(ngx.HTTP_UNAUTHORIZED)
+    ngx.log(ngx.WARN, "No session_id in cookies")
+    return ngx.exit(ngx.HTTP_UNAUTHORIZED)
 end
 
 local cache_key = "avatar_" .. ngx.md5(session_id)
 local method = ngx.var.request_method
 
--- Обработка GET (download)
+-- Обработка GET запроса
 if method == "GET" then
-    local avatar_data = cache:get(cache_key)
-
-    if avatar_data then
-        local cached = cjson.decode(avatar_data)
-        local etag = ngx.var.http_if_none_match
-        if etag and etag == cached.etag then
-            ngx.exit(304)
+    local args = ngx.req.get_uri_args()
+    if args.download then
+        local cached = cache:get(cache_key)
+        if cached then
+            cached = cjson.decode(cached)
+            local etag = ngx.var.http_if_none_match
+            if etag and etag == cached.etag then
+                return ngx.exit(304)
+            end
+            ngx.header["Content-Type"] = cached.content_type
+            ngx.header["ETag"] = cached.etag
+            ngx.header["Cache-Control"] = "public, max-age=300"
+            ngx.print(cached.data)
+            return ngx.exit(200)
         end
 
-        ngx.header["Content-Type"] = cached.content_type
-        ngx.header["ETag"] = cached.etag
-        ngx.header["Cache-Control"] = "public, max-age=300"
-        ngx.print(cached.data)
-        ngx.exit(200)
+        ngx.ctx.cache_key = cache_key
+        ngx.ctx.should_cache = true
     end
-
-    ngx.ctx.cache_key = cache_key
     return
 end
 
--- Обработка PUT (upload)
+-- Обработка PUT запроса
 if method == "PUT" then
     cache:delete(cache_key)
     ngx.log(ngx.NOTICE, "Purged avatar cache for: ", cache_key)
